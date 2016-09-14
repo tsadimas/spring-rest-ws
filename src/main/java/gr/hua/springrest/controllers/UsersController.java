464a1784f0dacedd9c0cbab2e791790a0c57d814
tsadimas <tsadimas@hua.gr>
2016-09-14 17:16:30 +0300

    Added support to return userslist in xml

--- src/main/java/gr/hua/springrest/controllers/UsersController.java
+++ src/main/java/gr/hua/springrest/controllers/UsersController.java
@@ -1,5 +1,9 @@
 package gr.hua.springrest.controllers;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -7,20 +11,40 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import gr.hua.springrest.models.User;
-
+import gr.hua.springrest.models.UserList;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UsersController {
 	
+	@Autowired
+	UserList userList;
+
 	@RequestMapping(value = "user/{userId:\\d+}", method = RequestMethod.GET)
-	public User getuser(@PathVariable("userId") int userId){
-		 User user =  new User();
-		 user.setId(userId);
-		 
-		 return user;
+	public User getuser(@PathVariable("userId") int userId) {
+		User user = new User();
+		user.setId(userId);
+		user.setName("Argiris");
+
+		return user;
+	}
+
+	@RequestMapping(value = "user/all", method= RequestMethod.GET)
+	public UserList getUsers() {
+		List<User> userlist = new ArrayList<User>();
+		User user1 = new User();
+		user1.setId(1);
+		user1.setName("Argiris");
+		userlist.add(user1);
+
+		User user2 = new User();
+		user2.setId(2);
+		user2.setName("Iason");
+		userlist.add(user2);
+
+		this.userList.setUserList(userlist);
+		return this.userList;
+
 	}
 
 }
-
-
---
+++ src/main/java/gr/hua/springrest/models/UserList.java
@@ -0,0 +1,22 @@
+package gr.hua.springrest.models;
+
+import java.util.List;
+
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement(name = "UserList")
+public class UserList {
+	
+	List<User> userList;
+
+	@XmlElement(name = "User")
+	public List<User> getUserList() {
+		return userList;
+	}
+
+	public void setUserList(List<User> userList) {
+		this.userList = userList;
+	}
+
+}
