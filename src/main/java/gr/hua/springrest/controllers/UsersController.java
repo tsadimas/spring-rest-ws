d18251c99c043ef19ef0a7aa4fcec8ddd0b01a5e
tsadimas <tsadimas@hua.gr>
2016-09-15 09:51:10 +0300

    added post method

--- src/main/java/gr/hua/springrest/controllers/UsersController.java
+++ src/main/java/gr/hua/springrest/controllers/UsersController.java
@@ -2,12 +2,15 @@ package gr.hua.springrest.controllers;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import gr.hua.springrest.models.User;
@@ -29,7 +32,7 @@ public class UsersController {
 		return user;
 	}
 
-	@RequestMapping(value = "user/all", method= RequestMethod.GET)
+	@RequestMapping(value = "user/all", method= RequestMethod.GET,  produces = {"application/json","application/xml"})
 	public UserList getUsers() {
 		List<User> userlist = new ArrayList<User>();
 		User user1 = new User();
@@ -45,6 +48,30 @@ public class UsersController {
 		this.userList.setUserList(userlist);
 		return this.userList;
 
+	}
+	
+	@RequestMapping(value = "user/create", method= RequestMethod.POST)
+	public User createUser(
+			@RequestParam("name") String name,
+			@RequestParam("email") String email, 
+			@RequestParam(value="country", required=false) String country,
+			@RequestParam("password") String password,
+			@RequestParam(value="phone", required=false) String phone){
+		User user= new User();
+		user.setName(name);
+		user.setEmail(email);
+		user.setPassword(password);
+		user.setCountry(country);
+		user.setPhone(phone);
+		//create random number to simulate a database entry
+		Random random = new Random();
+		user.setId(random.nextInt(10 - 2 + 1) + 2);
+		return user;
+	}
+	
+	@RequestMapping(value = "user/createjson", method= RequestMethod.POST)
+	public User createUserfromJson(@RequestBody User user){
+		return user;
 	}
 
 }
